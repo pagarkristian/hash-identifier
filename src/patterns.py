e2b01734be66e12655cb6cc14626a6b8d4d6142b
pagarkristian <akunkerja23128@gmail.com>
2026-06-03 22:19:01 +0700

    Add hash pattern database

---
+++ src/patterns.py
@@ -0,0 +1,46 @@
+HASH_PATTERNS = [
+	{
+		"name" : "MD5" ,
+		"length" : 32 ,
+		"regex" : r"^[a-fA-F0-9]{32}$",
+		"description" : "Message Digest Algorithm 5 "
+} ,
+
+        {
+                "name" : "SHA1" ,
+                "length" : 40 ,
+                "regex" : r"^[a-fA-F0-9]{40}$",
+                "description" : "Secure Hash Algorithm 1 "
+} ,
+
+        {
+                "name" : "SHA224" ,
+                "length" : 56 ,
+                "regex" : r"^[a-fA-F0-9]{56}$",
+                "description" : "Secure Hash Algorithm 224 "
+} ,
+
+        {
+                "name" : "SHA256" ,
+                "length" : 64 ,
+                "regex" : r"^[a-fA-F0-9]{64}$",
+                "description" : "Secure Hash Algorithm 256 "
+} ,
+
+        {
+                "name" : "SHA384" ,
+                "length" : 96 ,
+                "regex" : r"^[a-fA-F0-9]{96}$",
+                "description" : "Secure Hash Algorithm 384 "
+} ,
+        {
+                "name" : "SHA512" ,
+                "length" : 128 ,
+                "regex" : r"^[a-fA-F0-9]{128}$",
+                "description" : "Secure Hash Alogrithm 512 "
+}
+
+
+
+
+]
