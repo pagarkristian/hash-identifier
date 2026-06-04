c52ec0fee19ccdef1327db283668795a281cb2c3
pagarkristian <akunkerja23128@gmail.com>
2026-06-04 21:57:37 +0700

    Fix stats.py

---
+++ src/stats.py
@@ -0,0 +1,32 @@
+import os
+
+
+def show_stats():
+
+    log_file = "logs/hash_identifier.log"
+
+    if not os.path.exists(log_file):
+        print("No logs found.")
+        return
+
+    stats = {}
+
+    with open(log_file, "r") as file:
+
+        for line in file:
+
+            if "TYPE=" not in line:
+                continue
+
+            hash_type = line.split("TYPE=")[1].strip()
+
+            stats[hash_type] = stats.get(hash_type, 0) + 1
+
+    total = sum(stats.values())
+
+    print("\nStatistics")
+    print("=" * 50)
+    print(f"Total Hashes Scanned : {total}")
+
+    for hash_type, count in stats.items():
+        print(f"{hash_type:<10} : {count}")
